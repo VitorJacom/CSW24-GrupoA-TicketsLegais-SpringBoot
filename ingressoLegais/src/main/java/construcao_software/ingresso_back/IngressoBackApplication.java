1a6bdd1fd85cc56b40f7ba48ab8bd0f898ae15fb
vitorjacom <vjds.vitor@gmail.com>
2024-09-26 18:01:44 -0300

    projeto

---
+++ ingressoLegais/src/main/java/construcao_software/ingresso_back/IngressoBackApplication.java
@@ -0,0 +1,13 @@
+package construcao_software.ingresso_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngressoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngressoBackApplication.class, args);
+	}
+
+}
---
+++ ingressoLegais/src/test/java/construcao_software/ingresso_back/IngressoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package construcao_software.ingresso_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngressoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
