01d240a9a1dd6e203b59fcd37068ad0c62d1db82
VitorJacom <vitor.jacom@edu.pucrs.br>
2024-10-08 18:25:37 -0300

    add banco e swagger

--- ingressoLegais/src/main/java/construcao_software/ingresso_back/infrastructure/persistence/entities/SampleEntity.java
+++ ingressoLegais/src/main/java/construcao_software/ingresso_back/infrastructure/persistence/entities/SampleEntity.java
@@ -1,6 +1,11 @@
 package construcao_software.ingresso_back.infrastructure.persistence.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,8 +13,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
+@Table(name = "sample_entity")
 public class SampleEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private Long id;
 
