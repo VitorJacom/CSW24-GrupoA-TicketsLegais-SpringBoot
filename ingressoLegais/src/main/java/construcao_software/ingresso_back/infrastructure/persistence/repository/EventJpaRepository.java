f4464d014c6f7e41a1ceb5e1b8fda9d6d13ed2e9
VitorJacom <vitor.jacom@edu.pucrs.br>
2024-10-21 22:47:31 -0300

    corrigindo erro de nome de variavel

--- ingressoLegais/src/main/java/construcao_software/ingresso_back/infrastructure/persistence/repository/EventJpaRepository.java
+++ ingressoLegais/src/main/java/construcao_software/ingresso_back/infrastructure/persistence/repository/EventJpaRepository.java
@@ -13,7 +13,7 @@ import construcao_software.ingresso_back.infrastructure.persistence.hybernate.mo
 public interface EventJpaRepository extends JpaRepository<EventModel, Long> {
     // Aqui você pode adicionar métodos de consulta personalizados, se necessário
 
-    @Query("SELECT e FROM EventModel e WHERE e.name LIKE %:name%")
-    Collection<EventModel> getAllByFilterName(@Param("name") String name);
+    @Query("SELECT e FROM EventModel e WHERE e.eventName LIKE %:eventName%")
+    Collection<EventModel> getAllByFilterName(@Param("eventName") String eventName);
 
 }
