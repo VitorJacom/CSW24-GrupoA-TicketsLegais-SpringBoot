8f7d1c0384902049fa6fd6bf4c59fa89e25677d8
VitorJacom <vjds.vitor@gmail.com>
2024-11-11 11:18:39 -0300

    fazendo dockerfile e outros

--- ingressoLegais/src/test/java/construcao_software/ingresso_back/IngressoBackApplicationTests.java
+++ ingressoLegais/src/test/java/construcao_software/ingresso_back/IngressoBackApplicationTests.java
@@ -2,12 +2,46 @@ package construcao_software.ingresso_back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+
+import construcao_software.ingresso_back.application.dtos.UserDTO;
+import construcao_software.ingresso_back.application.services.UserService;
+import construcao_software.ingresso_back.domain.entities.UserEntity;
+import construcao_software.ingresso_back.infrastructure.persistence.hybernate.models.UserModel;
+import construcao_software.ingresso_back.infrastructure.persistence.repository.UserJpaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
 
 @SpringBootTest
+@ComponentScan(basePackages = "construcao_software.ingresso_back")
 class IngressoBackApplicationTests {
+
+    @MockBean
+    private UserJpaRepository userJpaRepository;
+
+    @Autowired
+    private UserService userService;
 
 	@Test
 	void contextLoads() {
+        // Simulação de comportamento do método findById no mock
+        when(userJpaRepository.findById(1L)).thenReturn(Optional.of(new UserModel()));
+
+        // Chamada real do método userService, que usa o repositório mockado
+        Optional<UserDTO> result = userService.getUserByID(1L);
+
+        // Verificação de que o resultado não é vazio, conforme esperado
+        assertTrue(result.isPresent());
+
+        // Verificação do uso do método findById no mock
+        verify(userJpaRepository).findById(1L);
+    }
 	}
 
-}
