163c5a14b1422a13fab076d9d8c78e9d3b6e2cc6
VitorJacom <vjds.vitor@gmail.com>
2024-11-19 14:46:45 -0300

    build na aws

---
+++ .infra/outputs.tf
@@ -0,0 +1,31 @@
+output "lambda_bucket_name" {
+  value = aws_s3_bucket.lambda_bucket.id
+}
+
+output "api_id" {
+  value = aws_api_gateway_rest_api.api.id
+}
+
+output "proxy_resource_id" {
+  value = aws_api_gateway_resource.proxy.id
+}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "my_db_address" {
+  value = aws_db_instance.my_db.address
+}
+
+output "my_db_name" {
+  value = aws_db_instance.my_db.db_name
+}
+
+output "my_db_username" {
+  value = aws_db_instance.my_db.username
+}
+
+output "my_db_password" {
+  value = aws_db_instance.my_db.password
+}
